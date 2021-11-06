f87fbdbc916bfc88deb27529f18522f5f2128f50
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-11-05 22:44:58 -0300

    add inventory click listener

---
+++ src/main/java/dev/_2lstudios/hypermenus/listeners/InventoryClickListener.java
@@ -0,0 +1,12 @@
+package dev._2lstudios.hypermenus.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryClickListener implements Listener {
+    @EventHandler
+    public void onInventoryClick(final InventoryClickEvent event) {
+
+    }
+}
