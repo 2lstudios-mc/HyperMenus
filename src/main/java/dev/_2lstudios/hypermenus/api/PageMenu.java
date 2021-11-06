d9bd2c63279b7463c0b41302170c9553722cae09
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-11-06 10:59:37 -0300

    add prev & next to page menu

--- src/main/java/dev/_2lstudios/hypermenus/api/PageMenu.java
+++ src/main/java/dev/_2lstudios/hypermenus/api/PageMenu.java
@@ -1,6 +1,8 @@
 package dev._2lstudios.hypermenus.api;
 
 public class PageMenu extends Menu {
+    private Menu prevMenu;
+    private Menu nextMenu;
     private int page;
 
     public PageMenu(final int page) {
@@ -13,6 +15,26 @@ public class PageMenu extends Menu {
 
     public PageMenu setPage(final int page) {
         this.page = page;
+
+        return this;
+    }
+
+    public Menu getPrevMenu() {
+        return prevMenu;
+    }
+
+    public PageMenu setPrevMenu(Menu prevMenu) {
+        this.prevMenu = prevMenu;
+
+        return this;
+    }
+
+    public Menu getNextMenu() {
+        return nextMenu;
+    }
+
+    public PageMenu setNextMenu(Menu nextMenu) {
+        this.nextMenu = nextMenu;
 
         return this;
     }
