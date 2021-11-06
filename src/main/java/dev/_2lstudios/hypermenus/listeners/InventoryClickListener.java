ff4f9c2e5efa332b8ff1f382266da2152a1f8ce1
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-11-06 09:01:23 -0300

    implement inventory click listener

--- src/main/java/dev/_2lstudios/hypermenus/listeners/InventoryClickListener.java
+++ src/main/java/dev/_2lstudios/hypermenus/listeners/InventoryClickListener.java
@@ -3,10 +3,34 @@ package dev._2lstudios.hypermenus.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+import dev._2lstudios.hypermenus.api.HyperMenusAPI;
+import dev._2lstudios.hypermenus.api.Menu;
+import dev._2lstudios.hypermenus.api.MenuItem;
 
 public class InventoryClickListener implements Listener {
+    private final HyperMenusAPI api;
+
+    public InventoryClickListener(final HyperMenusAPI api) {
+        this.api = api;
+    }
+
     @EventHandler
     public void onInventoryClick(final InventoryClickEvent event) {
+        final Inventory inventory = event.getClickedInventory();
+        final Menu menu = api.getMenu(inventory);
 
+        if (menu != null) {
+            event.setCancelled(true);
+            menu.onClick(event);
+            
+            final int slot = event.getSlot();
+            final MenuItem menuItem = menu.getItem(slot);
+
+            if (menuItem != null) {
+                menuItem.onClick(event);
+            }
+        }
     }
 }
