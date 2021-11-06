35af6e534061f44d76fb0b9a73a174c8cfe625da
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-11-06 10:41:24 -0300

    register inventory listeners

--- src/main/java/dev/_2lstudios/hypermenus/HyperMenus.java
+++ src/main/java/dev/_2lstudios/hypermenus/HyperMenus.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev._2lstudios.hypermenus.api.HyperMenusAPI;
+import dev._2lstudios.hypermenus.listeners.InventoryClickListener;
+import dev._2lstudios.hypermenus.listeners.InventoryCloseListener;
 import dev._2lstudios.hypermenus.listeners.PlayerJoinListener;
 import dev._2lstudios.hypermenus.listeners.PlayerQuitListener;
 
@@ -20,6 +22,8 @@ public class HyperMenus extends JavaPlugin {
 
         HyperMenus.api = new HyperMenusAPI();
 
+        pluginManager.registerEvents(new InventoryClickListener(api), this);
+        pluginManager.registerEvents(new InventoryCloseListener(), this);
         pluginManager.registerEvents(new PlayerJoinListener(), this);
         pluginManager.registerEvents(new PlayerQuitListener(), this);
     }
