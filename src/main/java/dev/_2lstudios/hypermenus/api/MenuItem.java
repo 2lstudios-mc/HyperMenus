ca9fd8f4bfbd314a05452ede12193c8140cd44f3
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-11-08 10:31:02 -0300

    add new get methods

--- src/main/java/dev/_2lstudios/hypermenus/api/MenuItem.java
+++ src/main/java/dev/_2lstudios/hypermenus/api/MenuItem.java
@@ -15,6 +15,11 @@ public class MenuItem {
     private Material type = Material.STONE;
     private String name = null;
     private List<String> lore = null;
+    private int amount;
+
+    public Material getType() {
+        return type;
+    }
 
     public MenuItem setType(final Material type) {
         this.type = type;
@@ -22,10 +27,28 @@ public class MenuItem {
         return this;
     }
 
+    public int getAmount() {
+        return amount;
+    }
+
+    public MenuItem setAmount(final int amount) {
+        this.amount = amount;
+
+        return this;
+    }
+
+    public String getDisplayname() {
+        return name;
+    }
+
     public MenuItem setDisplayname(final String name) {
         this.name = name;
 
         return this;
+    }
+
+    public List<String> getLore() {
+        return lore;
     }
 
     public MenuItem setLore(final List<String> lore) {
@@ -41,6 +64,7 @@ public class MenuItem {
         itemMeta.setDisplayName(PlaceholderAPIHook.replace(player, ColorsHook.replace(name)));
         itemMeta.setLore(PlaceholderAPIHook.replace(player, ColorsHook.replace(lore)));
         item.setItemMeta(itemMeta);
+        item.setAmount(amount);
 
         return item;
     }
